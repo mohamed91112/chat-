91a7ddf178d4cfe76e7521304ebd09251cd54b24
mohamed91112 <mohamedbenighil82@gmail.com>
2024-05-03 19:51:54 +0200

    Add files via upload

---
+++ dus.js
@@ -0,0 +1,54 @@
+
+function sendMessage() {
+     var messageInput =
+document.getElemenByld(messageInput)
+
+
+    var  message =
+    messageInput.value;
+
+     if (message.trim() !=="") {
+      var messegesDiv =
+document.getElementById("message");
+
+         var messageElement =
+         document.createdElement("div");
+                  messageElement.textContent
+= message ;
+
+ messagesDiv.appenChild(messageElement);
+        messageInput.value = "";
+     }
+    }
+     
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
